f0e65e059f5e362df51462f0bf3e8e4f768e7b33
Grant Taggart <granttaggartsf@gmail.com>
2021-09-22 12:00:37 -0700

    Write README and complete testing

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # highest-profit
+
+## Table of Contents
+  1. [Input](#Input)
+  2. [Output](#Output)
+  3. [Code Details]()
+  4. [Testing]()
+  5. [Requirements]()
+  6. [Usage]()
+
+## Input
+
+There is no input to the ```printProfitData``` function itself, but it does read from the ```data.csv``` file, which is provided with the repo. The ```data.csv``` has rows with headers labeled as follows and in the following order:
+  - Year
+  - Rank
+  - Company
+  - Revenue (in millions)
+  - Profit (in millions)
+
+## Output
+
+The ```printProfitData``` function doesn't have any return values, but it does have 4 side effects:
+   - printing out the total number of rows of data (excluding the header) in ```data.csv```
+   - printing out the number of rows containing valid numeric profits
+   - printing out the top 20 entries ranked in order of highest profit
+   - generating a JSON file (```data2.json```) that contains all of the entries from the csv file that have valid profit numbers, ordered from highest profit to lowest profit
+
+## Code Details
+
+  - the function, ```printProfitData``` is contained within the app.js file
+  - it does the following:
+    1. Reads the ```data.csv``` file
+    2. Prints the number of lines of data within the file (excluding the header)
+    3. Iterates over each line from the file and reformats the data into an object
+    4. Pushes objects with valid profit data into an array
+    5. Prints the number of entries in ```data.csv``` with valid profit data
+    6. Writes the valid entries to a JSON file called ```data2.json```, ordered from greatest profit to least profit(loss)
+    7. Prints out the top 20 entries with the highest profit
+
+## Testing
+
+  The tests are contained in the ```test.js``` file, and test for the following:
+    - a JSON file is created when the printProfitData function is called
+    - that the function prints three answers
+
+## Requirements
+
+  Node.js
+
+## Usage
+
+  1. Clone repo from Github
+  2. Run ```npm install``` to install dependencies
+  3. Run script with ```bash run.sh```
+  4. To test, delete ```data2.json``` file (if one exists) and run ```npm run test```
+
+
+
--- test.js
+++ test.js
@@ -2,25 +2,51 @@ const { expect } = require('chai');
 const fs = require('fs');
 const { printProfitData } = require('./app.js');
 
-describe('test for a JSON file', () => {
-  //Part 3: Test confirms the output of a file called data2.json
-    //I was trying to figure out if there was a way of counting console.log calls
-    //from within the test and without returning a counter from printProfitData
-    //but I wasn't able to find anything indicating that there's a way of doing that
-    //and it didn't seem like a legitimate test if the counter wasn't within the test itself.
+describe('test for a JSON file and correct number of answers', () => {
+  //Part 3: Test confirms the output of a file called data2.json and that the function prints three answers
     let createJSON;
     let fileLength;
+    let counter = 0;
+    //function below based on this Stack Overflow post: https://stackoverflow.com/questions/29775761/way-to-save-console-count-as-an-integer
+    function takeOverConsole() {
+      var console = global.console
+      if (!console) return
+      function intercept(method) {
+        var original = console[method]
+        console[method] = function () {
+          var message = Array.prototype.slice.apply(arguments).join(' ')
+          // do sneaky stuff
+          if (original.call) {
+            // Do this for normal browsers
+            original.call(console, message)
+          } else {
+            // Do this for IE
+            original(message)
+          }
+          counter++;
+        }
+      }
+      var methods = ['log', 'warn', 'error', 'count']
+      for (var i = 0; i < methods.length; i++)
+        intercept(methods[i])
+    }
 
     before(async () => {
+      takeOverConsole();
       createJSON = await printProfitData();
       fileLength = await JSON.parse(fs.readFileSync('./data2.json', 'utf8')).length;
     })
 
     describe('JSON exists and has the correct number of entries', () => {
-      it('it should have 25131 profit entries in the file', (done) => {
+      it('should have 25131 profit entries in the file', (done) => {
         expect(fileLength).to.equal(25131);
         done();
       });
+
+      it('should print three answers', (done) => {
+        expect(counter).to.equal(3);
+        done();
+      })
     })
 
 
